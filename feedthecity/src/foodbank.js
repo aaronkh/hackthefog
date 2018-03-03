0743914751077e3961e4200be69992f826f70429
Ryan <rykwong1@s.sfusd.edu>
2018-03-03 15:46:56 -0800

    list of address and items

--- feedthecity/src/foodbank.js
+++ feedthecity/src/foodbank.js
@@ -1,14 +1,48 @@
-let food = 
+let food = [
 	{"location": {
 		"address" : "75 Fast Lane",
 	 	"coord": [525,124]
 	}, 
 	 "category" : "fruit",
 	 "picture" : "http://noteablemusictherapy.com/wp-content/uploads/2017/09/apple.jpg",
-	 "description" : "apple",
+	 "item" : "apples",
+	 "description" : "Fugi Apples",
 	 "id" : 12193049,
 	 "contact" : "415-324-1338"
-}
+}, 
+{"location": {
+		"address" : "75 Fast Lane",
+	 	"coord": [525,124]
+	}, 
+	 "category" : "fruit",
+	 "picture" : "http://noteablemusictherapy.com/wp-content/uploads/2017/09/apple.jpg",
+	 "item" : "apples",
+	 "description" : "Fugi Apples",
+	 "id" : 12193049,
+	 "contact" : "415-324-1338"
+},
+{"location": {
+		"address" : "75 Fast Lane",
+	 	"coord": [525,124]
+	}, 
+	 "category" : "fruit",
+	 "picture" : "http://noteablemusictherapy.com/wp-content/uploads/2017/09/apple.jpg",
+	 "item" : "apples",
+	 "description" : "Fugi Apples",
+	 "id" : 12193049,
+	 "contact" : "415-324-1338"
+},
+{"location": {
+		"address" : "1949 The Way",
+	 	"coord": [525,124]
+	}, 
+	 "category" : "fruit",
+	 "picture" : "http://noteablemusictherapy.com/wp-content/uploads/2017/09/apple.jpg",
+	 "item" : "apples",
+	 "description" : "Fugi Apples",
+	 "id" : 12193049,
+	 "contact" : "415-324-1338"
+}]
 
 food = JSON.stringify(food)
 
--- feedthecity/src/list.js
+++ feedthecity/src/list.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
-class List extends Component{
+import {food} from './foodbank.js'
+
+class ListItems extends Component{
 
 	constructor(props){
 		super(props)
 		this.state = {}
 	}
+
+	render(){
+		let temp = JSON.parse(food)
+		let addresses = []
+		return temp.map((obj, i) => {
+			let locationAddress = ''
+			if(!addresses.includes(obj.location.address)){
+				locationAddress = obj.location.address
+				addresses.push(obj.location.address)
+			}
+			return(
+				<div className="center-text">
+					<div>
+						<font size = "5" >
+							{locationAddress}
+						</font>
+					</div>
+					<div onclick="fn()">
+						Item: {obj.item}
+
+					</div>
+					<div>
+						Category: {obj.category}
+					</div>
+				</div>
+			)
+		})
+	}
 }
+export default ListItems
