223c028e5d1f6e989065fd57eee42488c01d9a27
Ryan <rykwong1@s.sfusd.edu>
2018-03-03 13:44:52 -0800

    data

---
+++ feedthecity/src/foodbank.js
@@ -0,0 +1,25 @@
+let food = 
+	{"location": {
+		"address" : "75 Fast Lane",
+	 	"coord": [525,124]
+	}, 
+	 "category" : "fruit",
+	 "picture" : "http://noteablemusictherapy.com/wp-content/uploads/2017/09/apple.jpg",
+	 "description" : "apple",
+	 "id" : 12193049,
+	 "contact" : "415-324-1338"
+}
+
+food = JSON.stringify(food)
+
+let account = 
+	{"id" : 12193049,
+	 "contact" : "415-324-1338",
+	 "location": {
+		"address" : "75 Fast Lane",
+	 	"coord": [525,124]
+		} 
+	}
+account = JSON.stringify(account)
+
+export {food, account}
